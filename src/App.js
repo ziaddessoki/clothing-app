ad2a2a3c3e070e48b03814951000cff81e2197be
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2021-08-20 02:17:40 -0400

    homepage stylingh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Homepage } from './Homepage.component';
+import { Homepage } from './pages/Homepage.component';
 
 
 import './App.css';
---
+++ src/pages/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+import { Directory } from '../components/directory/Directory.component'
+
+export const Homepage = () => {
+    return (
+        <div className="homePage">
+            <Directory />
+        </div>
+    )
+}
