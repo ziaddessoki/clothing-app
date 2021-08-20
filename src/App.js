60de5b1d6fd79e3f53e2b5606f569e70f9a7979f
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2021-08-20 00:06:34 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Homepage } from './Homepage.component';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
