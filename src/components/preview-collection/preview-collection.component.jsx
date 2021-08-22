64e2ca169cb11f5c6ff892d68d3464f2450f5b7a
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2021-08-22 19:01:25 -0400

    shop preview

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './preview-collection.styles.scss'
 
-export default function PreviewCollection({ title, items }) {
+import CollectionItem from '../collection-item/collectionItem.component'
+
+const PreviewCollection = ({ title, items }) => {
     return (
         <div className='collection-preview'>
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
-                {items.map(item => (
-                    <div key={item.id}>{item.name}</div>
-                ))}
+                {items.filter((item, idx) => idx < 4)
+                    .map(({ id, ...otherProps }) => (
+                        <CollectionItem key={id} {...otherProps} />
+                    ))}
             </div>
         </div>
     )
 }
+
+export default PreviewCollection
