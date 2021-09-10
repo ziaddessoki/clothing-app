d735221d31ac4ba86af7408413bc9bd7267142ce
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2021-09-09 22:42:07 -0400

    redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,27 @@
 import { combineReducers } from "redux";
+import { persistReducer } from 'redux-persist'
+//to access localStorage 
+import { storage } from 'redux-persist/lib/storage'
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    //basically the reduce that wanted to be save to localStorage via persist
+    whitelist: ['cart']
+}
+
+
+// export default combineReducers({
+//     user: userReducer,
+//     cart: cartReducer
+// })
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistConfig(persistReducer, rootReducer)
