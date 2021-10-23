6051068f2e907d47de837cd3f1d4bffde9e1286f
Zee(Ziad) Eldesouky <ziad.dessoki@yahoo.com>
2021-10-23 03:42:11 -0400

    Done with major functionality

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,24 @@
 import { createStore, applyMiddleware } from "redux";
-import { persistStore } from 'redux-persist'
+import { persistStore } from "redux-persist";
 //to log state and actin in console
 import logger from "redux-logger";
 //for Redux div tools
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import rootReducer from './root-reducer'
+import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 //using persist to cache data, same as using localStorage
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
 // export default { store, persistor }
